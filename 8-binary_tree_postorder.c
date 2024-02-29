20ea0750f1b9416a7a81c8640021abce8cb420e7
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-02-29 01:44:45 +0000

    Add task 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,15 +7,15 @@
  */
 void visit_node(const binary_tree_t *node, void (*func)(int))
 {
-        if (node != NULL && func != NULL)
-        {
-                binary_tree_t *left_ptr = node->left;
-                binary_tree_t *right_ptr = node->right;
+	if (node != NULL && func != NULL)
+	{
+		binary_tree_t *left_ptr = node->left;
+		binary_tree_t *right_ptr = node->right;
 
-                visit_node(left_ptr, func);
-                visit_node(right_ptr, func);
+		visit_node(left_ptr, func);
+		visit_node(right_ptr, func);
 		func(node->n);
-        }
+	}
 }
 
 /**
@@ -25,6 +25,6 @@ void visit_node(const binary_tree_t *node, void (*func)(int))
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree != NULL || func != NULL)
-                visit_node(tree, func);
+	if (tree != NULL || func != NULL)
+		visit_node(tree, func);
 }
