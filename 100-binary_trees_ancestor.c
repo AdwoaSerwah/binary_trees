a1a60058caaa6681077b248aa2edf0e8ffa039b2
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-03-01 00:22:45 +0000

    Add task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
-
 /**
- * find_depth1 - Finds the depth of a node
+ * find_depth - Finds the depth of a node
  * @node: Node
  *
  * Return: depth
  */
-int find_depth1(const binary_tree_t *node)
+int find_depth(const binary_tree_t *node)
 {
 	int count = 0;
 	const binary_tree_t *ptr = node;
 
-	while (ptr->parent != NULL)
+	while (ptr != NULL)
 	{
 		count++;
 		ptr = ptr->parent;
@@ -29,44 +28,33 @@ int find_depth1(const binary_tree_t *node)
 binary_tree_t *binary_trees_ancestor(
 		const binary_tree_t *first, const binary_tree_t *second)
 {
-	int first_depth = 0, second_depth = 0;
+	int first_depth, second_depth;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
-	if (first->parent == NULL)
-		return ((binary_tree_t *) first);
-	if (second->parent == NULL)
-		return ((binary_tree_t *) second);
-	first_depth = find_depth1(first), second_depth = find_depth1(second);
-	if (first_depth != second_depth)
+
+	first_depth = find_depth(first);
+	second_depth = find_depth(second);
+
+	while (first_depth > second_depth)
 	{
-		if (first_depth > second_depth)
-		{
-			while (first_depth != second_depth)
-			{
-				if (first->parent == second)
-					return (first->parent);
-				first_depth--;
-				first = first->parent;
-			}
-		}
-		else
-		{
-			while (second_depth != first_depth)
-			{
-				if (second->parent == first)
-					return (second->parent);
-				second_depth--;
-				second = second->parent;
-			}
-		}
+		first = first->parent;
+		first_depth--;
 	}
-	while (first_depth > 0)
+
+	while (second_depth > first_depth)
 	{
-		if (first->parent == second->parent)
-			return (first->parent);
+		second = second->parent;
+		second_depth--;
+	}
+
+	while (first != second)
+	{
+		if (first == NULL || second == NULL)
+			return (NULL); /* No common ancestor */
 		first = first->parent;
 		second = second->parent;
 	}
-	return (NULL);
+
+	return ((binary_tree_t *)first);
 }
