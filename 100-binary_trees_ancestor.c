6fb8047e11b215ee5ab9c5bb3805c6b196aeec58
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-03-01 00:55:03 +0000

    Add task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,7 +4,7 @@
  * find_depth - Finds the depth of a node
  * @node: Node
  *
- * Return: depth
+ * Return: count
  */
 int find_depth(const binary_tree_t *node)
 {
@@ -33,29 +33,24 @@ binary_tree_t *binary_trees_ancestor(
 
 	if (first == NULL || second == NULL)
 		return (NULL);
-
 	first_depth = find_depth(first);
 	second_depth = find_depth(second);
-
 	while (first_depth > second_depth)
 	{
 		first = first->parent;
 		first_depth--;
 	}
-
 	while (second_depth > first_depth)
 	{
 		second = second->parent;
 		second_depth--;
 	}
-
 	while (first != second)
 	{
 		if (first == NULL || second == NULL)
-			return (NULL); /* No common ancestor */
+			return (NULL);
 		first = first->parent;
 		second = second->parent;
 	}
-
 	return ((binary_tree_t *)first);
 }
