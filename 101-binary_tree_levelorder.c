36e72426a8eb8342c72675ad1bb60e3adb7157d9
AdwoaSerwah <138702029+AdwoaSerwah@users.noreply.github.com>
2024-06-06 20:59:09 +0000

    Add

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,37 +1,84 @@
 #include "binary_trees.h"
 
 /**
- * visit_node - Visits node
- * @node: Node to visit
- * @func: Function pointer
+ * struct qnode - Queue node
+ * @node: Binary tree node
+ * @next: Next node in the queue
  */
-void visit_node(const binary_tree_t *node, void (*func)(int))
+typedef struct qnode
 {
-	if (node != NULL && func != NULL)
+	const binary_tree_t *node;
+	struct qnode *next;
+} qnode_sk;
+
+/**
+ * enqueue - Adds node to queue
+ * @queue: Queue
+ * @node: Node
+ */
+void enqueue(qnode_sk **queue, const binary_tree_t *node)
+{
+	qnode_sk *new_skb = malloc(sizeof(qnode_sk));
+	qnode_sk *tmp_skb = *queue;
+
+	if (!new_skb)
+		return;
+	new_skb->node = node;
+	new_skb->next = NULL;
+
+	if (!*queue)
 	{
-		binary_tree_t *left_ptr = node->left;
-		binary_tree_t *right_ptr = node->right;
-
-		if (left_ptr != NULL)
-			func(left_ptr->n);
-		if (right_ptr != NULL)
-			func(right_ptr->n);
-
-		visit_node(left_ptr, func);
-		visit_node(right_ptr, func);
+		*queue = new_skb;
+	}
+	else
+	{
+		while (tmp_skb->next)
+			tmp_skb = tmp_skb->next;
+		tmp_skb->next = new_skb;
 	}
 }
 
 /**
- * binary_tree_levelorder - Traverses a binary tree using levelorder traversal
- * @tree: Pointer to root node
- * @func: Pointer to a function
+ * dequeue - Removes a node from the front of the queue
+ * @queue: The queue to remove from
+ *
+ * Return: The binary tree node removed
+ */
+const binary_tree_t *dequeue(qnode_sk **queue)
+{
+	qnode_sk *tmp_skb;
+	const binary_tree_t *node;
+
+	if (!*queue)
+		return (NULL);
+	tmp_skb = *queue;
+	node = tmp_skb->node;
+	*queue = (*queue)->next;
+	free(tmp_skb);
+	return (node);
+}
+
+/**
+ * binary_tree_levelorder - Goes through a binary tree using level-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL && func != NULL)
+	qnode_sk *queue = NULL;
+	const binary_tree_t *current;
+
+	if (!tree || !func)
+		return;
+
+	enqueue(&queue, tree);
+
+	while ((current = dequeue(&queue)))
 	{
-		func(tree->n);
-		visit_node(tree, func);
+		func(current->n);
+		if (current->left)
+			enqueue(&queue, current->left);
+		if (current->right)
+			enqueue(&queue, current->right);
 	}
 }
